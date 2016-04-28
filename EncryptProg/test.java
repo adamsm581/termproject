de36ffd1381e66f6517d04bca8793ecefbe2dec8
adamsm185 <adamsm581@gmail.com>
2016-04-28 16:25:41 -0400

    Added encryption test program. Could potentially be used

---
+++ EncryptProg/test.java
@@ -0,0 +1,30 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+public class test
+{
+  public static void main(String[] arguments) throws Exception
+  {
+    String encryptionKey = "PaZgWaeEmAaaeRuJ";
+    String plainText = "Hello world!";
+    AdvancedEncryptionStandard advancedEncryptionStandard = new AdvancedEncryptionStandard(
+            encryptionKey);
+    long starttime1, starttime2, endtime1, endtime2, duration1, duration2, durationtotal;
+    starttime1 = System.nanoTime();
+    String cipherText = advancedEncryptionStandard.encrypt(plainText);
+    endtime1 = System.nanoTime();
+    duration1 = (endtime1 - starttime1);
+    starttime2 = System.nanoTime();
+    String decryptedCipherText = advancedEncryptionStandard.decrypt(cipherText);
+    endtime2 = System.nanoTime();
+    duration2 = (endtime2 - starttime2);
+    durationtotal = duration1 + duration2;
+    System.out.println("Original text: " + plainText);
+    System.out.println("Time taken to encrypt: " + duration1 + " nano seconds.");
+    System.out.println("Cipher text: " + cipherText);
+    System.out.println("Time taken to decrypt: " + duration2 + " nano seconds.");
+    System.out.println("Decrypted text: " + decryptedCipherText);
+    System.out.println("Total time taken: " + durationtotal + " nano seconds.");
+  }
+}
